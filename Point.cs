a8613ea62de3809120e6744439b31a89daf87825
Matt Halvorson <halvorson34.mh@gmail.com>
2021-01-09 14:51:51 -0600

    created Point constructor as well as DistanceTo method in Point.cs

--- Point.cs
+++ Point.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace TreehouseDefense
 {
     class Point
@@ -9,6 +11,24 @@ namespace TreehouseDefense
         {
            X = x;
            Y = y;
+        }
+      
+        public int DistanceTo(int x, int y)
+        {
+          
+//           Cartesian distance formula long way
+          
+//           int xDiff = X - x;
+//           int yDiff = Y - y;
+//          
+//           int xDiffSquared = xDiff * xDiff;
+//           int yDiffSquared = yDiff * yDiff;
+//          
+//           return (int)Math.Sqrt(xDiffSquared + yDiffSquared);
+          
+          
+//           Cartesian distance formula refactored
+             return (int)Math.Sqrt(Math.Pow(X-x, 2) + Math.Pow(Y-y, 2));
         }
      }
 }
